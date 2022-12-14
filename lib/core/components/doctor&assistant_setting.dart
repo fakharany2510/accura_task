3acedc9e428a7c58dce3f4bc0c53740a5750fabf
mahmoud <mahmoud180280@fci.bu.edu.eg>
2022-12-15 01:27:51 +0200

    last push 9

--- lib/core/components/doctor&assistant_setting.dart
+++ lib/core/components/doctor&assistant_setting.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sizer/sizer.dart';
 
-
 class doctorAndAssestantSetting extends StatefulWidget {
   const doctorAndAssestantSetting({Key? key}) : super(key: key);
 
