559d6159f012c3d10d63bfc82f15e8078fe4f8e0
fakharany2510 <fakharany00@gmail.com>
2022-12-15 02:41:19 +0200

    last push 0

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
-
 class AppColors {
   static Color white = HexColor('#FFFFFF');
   static Color blue = HexColor('#3354F4');
